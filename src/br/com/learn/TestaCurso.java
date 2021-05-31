b84a1cae643106683714c771b358230a6e96a60b
João Gutierre <gutierrejoao@gmail.com>
2021-05-31 19:36:53 -0300

    feat: create element inside unmodifiable list witch specif method.

--- src/br/com/learn/TestaCurso.java
+++ src/br/com/learn/TestaCurso.java
@@ -6,13 +6,22 @@ public class TestaCurso {
 		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
         
 		List<Aula> aulas = javaColecoes.getAulas();
-        
 		System.out.println(aulas);
+        
+		/*Acessando o próprio método getters para inserir itens na lista - *DEPRECATED*.
         
         javaColecoes.getAulas().add(new Aula("Trabalhando com Arraylist", 21));
         javaColecoes.getAulas().add(new Aula("Listas de Objetos", 20));
         javaColecoes.getAulas().add(new Aula("Relacionamentos com coleções", 25));
         
-        System.out.println(aulas);
+        System.out.println(aulas);*/
+       
+        //Inserindo itens na lista à partir de um método específico para isto.
+      
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Listas de Objetos, 20", 20));
+        javaColecoes.adiciona(new Aula("Relacionamentos com coleções", 25));
+        
+        System.out.println(javaColecoes.getAulas());
 	}
 }
