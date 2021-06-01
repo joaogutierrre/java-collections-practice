c344aac9265557ee9c24334eca0d8385fb36ff1c
João Gutierre <gutierrejoao@gmail.com>
2021-06-01 17:13:43 -0300

    fix: refactor and display class without sorting.

--- src/br/com/learn/TestaCurso2.java
+++ src/br/com/learn/TestaCurso2.java
@@ -5,24 +5,29 @@ public class TestaCurso2 {
 	public static void main(String[] args) {
 		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
 		
-		//List<Aula> aulas = javaColecoes.getAulas();
-		
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 	    javaColecoes.adiciona(new Aula("Listas de Objetos", 20));
 	    javaColecoes.adiciona(new Aula("Relacionamentos com coleções", 25));
 	    
-	    System.out.println(javaColecoes.getAulas());
 	    
-	    List<Aula> aulasImutaveis = javaColecoes.getAulas();
-	    
+	    /*Old way to display courses & classes lists
+	     * 
+	     * List<Aula> aulasImutaveis = javaColecoes.getAulas();
 	    List<Aula> aulas = new ArrayList<>(aulasImutaveis);
-	    
-	    Collections.sort(aulas);
-	    System.out.println(aulas);
-	    
 	    int tempo = javaColecoes.getTempoTotal();
 	    String nome = javaColecoes.getNome();
+	    Collections.sort(aulas);
+	    
 	    System.out.println("Curso: " + nome + ". Tempo total: " + tempo +" hrs.");
+	    System.out.println(aulas);*/
+	    
+	    
+	    /*New way to display courses & classes lists"/
+	     * 
+	     */
+	    
+	    String cursoAulas = javaColecoes.toString();
+	    System.out.println(cursoAulas);
 	}
 	
 }
