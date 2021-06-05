193bcf2627b9c6d3419fa75fc7bfc5f0a6357e2b
João Gutierre <gutierrejoao@gmail.com>
2021-06-05 16:03:15 -0300

    feat: implement course test class

--- src/br/com/learn/TestandoCursoComAluno.java
+++ src/br/com/learn/TestandoCursoComAluno.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class TestandoCursoComAluno {
 	public static void main(String[] args) {
-		Curso javaColecoes = new Curso("Dominando as coleções do Java",
+		Curso javaColecoes = new Curso("Dominando as coleï¿½ï¿½es do Java",
                 "Paulo Silveira");
 
         javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
         javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
-        javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+        javaColecoes.adiciona(new Aula("Modelando com coleï¿½ï¿½es", 24));
         
         Aluno a1 = new Aluno("Rodrigo Turini", 34672);
         Aluno a2 = new Aluno("Guilherme Silveira", 5617);
@@ -17,5 +17,10 @@ public class TestandoCursoComAluno {
         javaColecoes.matricula(a1);
         javaColecoes.matricula(a2);
         javaColecoes.matricula(a3);
+        
+        System.out.println("Os alunos matriculados sÃ£o: ");
+        javaColecoes.getAlunos().forEach(a->{
+        	System.out.println(a);
+        });
 	}
 }
