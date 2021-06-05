9f833612f607e27e151e8c1b86715aa94518a1c3
João Gutierre <gutierrejoao@gmail.com>
2021-06-05 15:29:17 -0300

    feat:create student list & method

--- src/br/com/learn/Curso.java
+++ src/br/com/learn/Curso.java
@@ -2,53 +2,51 @@ package br.com.learn;
 
 import java.util.*;
 
-public class Curso{
-    private String nome;
-    private String instrutor;
-    private int tempoTotal;
-    private List<Aula> aulas = new LinkedList<Aula>();
-    
+public class Curso {
+	private String nome;
+	private String instrutor;
+	private int tempoTotal;
+	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 
-    public Curso(String nome, String instrutor){
-        this.nome = nome;
-        this.instrutor = instrutor;
-    }
+	public Curso(String nome, String instrutor) {
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
 
-    public String getNome(){
-        return this.nome;
-    }
+	public String getNome() {
+		return this.nome;
+	}
 
-    public String getInstrutor(){
-        return this.instrutor;
-    }
+	public String getInstrutor() {
+		return this.instrutor;
+	}
 
-    public List<Aula> getAulas(){
-        return Collections.unmodifiableList(aulas);
-    }
-    
-    public void adiciona (Aula aula) {
-    	this.aulas.add(aula);
-    }
-    
-    public int getTempoTotal() {
-    	return this.aulas.stream().mapToInt(Aula::getTempo).sum();
-    }
-    
-    @Override
-    public String toString() {
-    	// TODO Auto-generated method stub
-    	return "[Curso: "
-    			+ this.getNome() +
-    			". Tempo total: "
-    			+ this.getTempoTotal() + 
-    			". Aulas: " 
-    			+ this.getAulas() ; 
-    }
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
+
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "[Curso: " + this.getNome() + ". Tempo total: " + this.getTempoTotal() + ". Aulas: " + this.getAulas();
+	}
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
-		
+
 	}
-    
-    
+
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
 }
