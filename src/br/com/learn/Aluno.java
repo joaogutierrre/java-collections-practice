57411131103cfbf824c0f4f30db8b7f7f3907fec
João Gutierre <gutierrejoao@gmail.com>
2021-06-05 15:59:40 -0300

    feat: create student class

--- src/br/com/learn/Aluno.java
+++ src/br/com/learn/Aluno.java
@@ -1,39 +1,25 @@
 package br.com.learn;
-import java.util.*;
 
 public class Aluno {
 	public String nomeAluno;
 	public int numeroMatricula;
-	private Set<Aluno> alunos = new HashSet<>();
-	
+
 	public Aluno(String nomeAluno, int numeroMatricula) {
-		super();
 		this.nomeAluno = nomeAluno;
 		this.numeroMatricula = numeroMatricula;
 	}
-	
+
 	public String getNomeAluno() {
 		return this.nomeAluno;
 	}
-	
+
 	public int getMatricula() {
 		return this.numeroMatricula;
-	}
-	
-	public void adicionaAluno(Aluno aluno) {
-		this.alunos.add(aluno);
-	}
-	
-	public Set<Aluno> getAlunos(){
-		return Collections.unmodifiableSet(alunos);
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return "[Nome do aluno: " 
-				+ this.getNomeAluno() + 
-				". Matrícula: " + 
-				this.getMatricula();
+		return "[Nome do aluno: " + this.getNomeAluno() + ". MatrÃ­cula: " + this.getMatricula();
 	}
 }
